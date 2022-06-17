dc79aae23f08496b34463d7d62835dfef31ab704
Fabrice Tuilliere <fabrice.tui@gmail.com>
2022-06-17 21:06:13 +0200

    add redo button on test page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  get '/matchs/destroy', to: 'matches#destroy_all', as: 'destroy_all'
   resources :offers do
     resources :matches
   end
